e0a398f19c68d8950d6fc32a4a3d27008827d285
Nicole <nicolelowsl@gmail.com>
2021-06-11 23:20:02 +0800

    Added events stack

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from "./screen/homeScreen";
 import EventsScreen from "./screen/eventScreen";
 import ContactsScreen from "./screen/contactsScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
 
 
 const Tab = createBottomTabNavigator();
@@ -12,7 +14,29 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            
+            //Set the icon based on which route it is (name of the tab)
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Events') {
+              iconName = 'list';
+            } else if (route.name === 'Contacts'){
+              iconName = focused?  'user': 'user-o';
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Events" component={EventsScreen} />
         <Tab.Screen name="Contacts" component={ContactsScreen} />
--- screen/homeScreen.js
+++ screen/homeScreen.js
@@ -1,10 +1,12 @@
 import * as  React from "react";
 import { Text, View } from "react-native";
+import { FontAwesome } from '@expo/vector-icons'; 
 
 export default function HomeScreen() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'lightgreen' }}>
         <Text>Home!</Text>
+        <FontAwesome name="home" size={45} color="blue" />
       </View>
     );
   }
